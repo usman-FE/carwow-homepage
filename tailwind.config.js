242347548c349e1475982b324750203d257f3c83
Muhammad Usman <ch.usman564@gmail.com>
2023-03-15 08:33:09 +0500

    Completed homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html'],
+  content: ['./*.html'],
   theme: {},
   plugins: [],
 };
@@ -10,5 +10,5 @@ bodyOverflow: function (modal) {
   document.querySelector('body').classList.remove('overflow-hidden', 'overflow-visible');
   document.querySelector('body').classList.add(modal ? 'overflow-hidden' : 'overflow-visible');
 }
-
+ff0020
 */
